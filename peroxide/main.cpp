2fa0cb723480cd9083d35adecd2172c2510637ae
Fuzzy Klein <fuzzyklein2@gmail.com>
2026-08-30 15:25:28 -0500

    Pattern stack problem.

--- peroxide/main.cpp
+++ peroxide/main.cpp
@@ -2,6 +2,10 @@
  * @file peroxide.cpp
  *
  * Peroxide class member functions.
+ *
+ * @bug Sometimes the log file is empty. Any log file that exists should have some content.
+ *      This might be due to debugging sessions messing it up somehow. I've seen it twice now.
+ *      In between sightings I saw a log file with content, so it's kind of a mystery.
  */
 
 #include "peroxide.hpp"
